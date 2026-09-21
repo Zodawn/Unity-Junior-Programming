9ce53c56150be8dbac87ecff95d37c12b0f7db24
Zodawn <cordaeclark@gmail.com>
2026-09-21 08:35:30 -0400

    1.4.7 Make the vihicle turn instead of sliding

--- Prototype1/Assets/Scripts/PlayerController.cs
+++ Prototype1/Assets/Scripts/PlayerController.cs
@@ -1,20 +1,27 @@
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class PlayerController : MonoBehaviour
 {
     public float speed = 10.0f;
     public float turnSpeed = 50.0f;
+    public InputAction moveAction;
+    public Vector2 moveInput;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        moveAction.Enable();
     }
 
     // Update is called once per frame 
     void Update()
     {
         // We'll move the vehicle forward based on the vertical input axis (W/S or Up/Down arrow keys)
-        transform.Translate(Vector3.forward * Time.deltaTime * speed);
-        transform.Translate(Vector3.right * Time.deltaTime * turnSpeed);
+
+        Vector2 moveInput = moveAction.ReadValue<Vector2>();
+        transform.Translate(Vector3.forward * Time.deltaTime * speed * moveInput.y);
+
+        transform.Rotate(Vector3.up * Time.deltaTime * turnSpeed * moveInput.x);
+        
     }
 }
