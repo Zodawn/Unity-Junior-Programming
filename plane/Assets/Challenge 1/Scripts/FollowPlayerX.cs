be27aa68652341e197f586abc5afb95d76427606
Zodawn <cordaeclark@gmail.com>
2026-09-24 08:42:18 -0400

    plaen

--- plane/Assets/Challenge 1/Scripts/FollowPlayerX.cs
+++ plane/Assets/Challenge 1/Scripts/FollowPlayerX.cs
@@ -1,21 +1,26 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class FollowPlayerX : MonoBehaviour
 {
     public GameObject plane;
     private Vector3 offset;
+    public InputAction moveAction;
+    public Vector2 moveInput;
 
     // Start is called before the first frame update
     void Start()
     {
-        offset = new Vector3(30, 0, 10);
+        offset = new Vector3(9, 3, 7);
+        moveAction.Enable();
     }
 
     // Update is called once per frame
     void Update()
     {
+        Vector2 moveInput = moveAction.ReadValue<Vector2>();
         transform.position = plane.transform.position + offset;
     }
 }
---
+++ plane/Assets/Challenge 1/Scripts/PlayerControllerX.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerControllerX : MonoBehaviour
+{
+    public float speed = 10f;
+    public float rotationSpeed = 50f;
+    private float verticalInput;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        moveAction.Enable();
+    }
+
+    public InputAction moveAction;
+
+    private Vector2 moveInput;
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(Vector3.forward * Time.deltaTime * speed);
+        // get the user's vertical input
+        verticalInput = Input.GetAxis("Vertical");
+
+        // move the plane forward/back based on vertical input
+        transform.Rotate(Vector3.left * Time.deltaTime * verticalInput * rotationSpeed);
+
+        // tilt the plane up/down based on vertical input
+        transform.Translate(Vector3.up * Time.deltaTime * verticalInput * speed);
+    }
+}
